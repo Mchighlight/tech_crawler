0adf013c3424d0139664915d0c3f6b252154dedb
Henry Huang <Mchighlight@gmail.com>
2019-04-21 20:39:56 +0800

    activate async

--- multitask/tasks.py
+++ multitask/tasks.py
@@ -29,11 +29,11 @@ def cronjob(data):
     article = [] 
     if crawler_info['site'] == 0 :
         number_of_article = round(crawler_info['article_num']/2)
-        bloomberg_article = crawler.apply_async(args=[1, number_of_article, USER_AGENT])
+        bloomberg_article = crawler.apply_async(args=[1, number_of_article])
         #bloomberg_article = crawler(1, number_of_article, USER_AGENT)
         if ( len(bloomberg_article) == 0 ):
             number_of_article = crawler_info['article_num']
-        tnw_article = crawler.apply_async(args=[2, number_of_article, USER_AGENT])
+        tnw_article = crawler.apply_async(args=[2, number_of_article])
         #tnw_article = crawler(2, number_of_article, USER_AGENT)
     
         article.extend(tnw_article)
