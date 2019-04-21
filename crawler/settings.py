083da89b6ddf356069468712200ef22c26c501cf
Henry Huang <Mchighlight@gmail.com>
2019-04-21 20:19:02 +0800

    modify setttings

--- crawler/settings.py
+++ crawler/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c$jqn!9y4rst($mgxga3t%k217)vvcl!@k(jd6bx%c==wwh8sa'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
